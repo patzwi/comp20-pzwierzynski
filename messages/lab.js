12556057a1b2c436b58037ca23dbdddf7813a125
patzwi <patrick.zwierzynski@tufts.edu>
2017-06-17 16:45:41 -0400

    Modified stylesheet and added js code to get messages from file -- part1

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,26 @@
 // Your JavaScript goes here...
+function displayData(jsonData) {
+  var messages = document.getElementById("messages");
+  var post = "";
+  jsonData.map((message) => {
+    var current_mess = "<p> (" + message.id + ") " + message.content;
+    current_mess += " : " + message.username + "</p>";
+    post += current_mess;
+  });
+  messages.innerHTML = post;
+}
+
+function parse() {
+  var xml = new XMLHttpRequest();
+  xml.open("GET", "data.json", true);
+  xml.onreadystatechange = function() {
+    if ((xml.readyState == 4) && (xml.status == 200)) {
+        //console.log("response received");
+        var jsonData = JSON.parse(xml.responseText);
+        displayData(jsonData);
+    } /* else {
+         console.log("waiting for response")
+    } */
+  };
+  xml.send();
+}
